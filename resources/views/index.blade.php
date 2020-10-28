4cdb5e721b1e336118ead9924786c21f6f56c46a
lorenzo-damico <loredam@hotmail.it>
2020-10-28 18:52:26 +0100

    Aggiunta link di collegamento fra pagine

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,17 +5,45 @@
     <title>Index</title>
   </head>
   <body>
-    
+
+    <div>
+      <a href="{{ route('books.create') }}">Inserisci un libro</a>
+    </div>
+
     @foreach ($books as $book)
 
       <div>
-        <h2>{{$book->title}}</h2>
-        <h3>{{$book->author}}</h3>
-        <small>{{$book->edition}}</small>
         <img src="{{$book->image}}" alt="copertina libro">
       </div>
 
+      <table>
+        <thead>
+          <tr>
+            <th>Titolo</th>
+            <th>Autore</th>
+            <th>Genere</th>
+            <th>Edizione</th>
+            <th>Pagine</th>
+            <th>Anno</th>
+            <th>ISBN</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{$book->title}}</td>
+            <td>{{$book->author}}</td>
+            <td>{{$book->genre}}</td>
+            <td>{{$book->edition}}</td>
+            <td>{{$book->pages}}</td>
+            <td>{{$book->year}}</td>
+            <td>{{$book->isbn}}</td>
+            <td><a href="{{ route('books.show', $book) }}">Vai al libro</a></td>
+          </tr>
+        </tbody>
+      </table>
+
     @endforeach
+
 
   </body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,22 +6,44 @@
   </head>
   <body>
 
-    <ul>
-      <li>{{$book->title}}</li>
-      <li>{{$book->author}}</li>
-      <li>{{$book->isbn}}</li>
-      <li>{{$book->genre}}</li>
-      <li>{{$book->edition}}</li>
-      <li>{{$book->pages}}</li>
-      <li>{{$book->year}}</li>
+    <div>
       <img src="{{$book->image}}" alt="copertina libro">
-    </ul>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Titolo</th>
+          <th>Autore</th>
+          <th>Genere</th>
+          <th>Edizione</th>
+          <th>Pagine</th>
+          <th>Anno</th>
+          <th>ISBN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{$book->title}}</td>
+          <td>{{$book->author}}</td>
+          <td>{{$book->genre}}</td>
+          <td>{{$book->edition}}</td>
+          <td>{{$book->pages}}</td>
+          <td>{{$book->year}}</td>
+          <td>{{$book->isbn}}</td>
+          <td><a href="{{ route('books.edit', $book) }}">Modifica</a></td>
+          <td><a href="{{ route('books.index') }}">Indice</a></td>
+        </tr>
+      </tbody>
+    </table>
+
 
     <form action="{{route('books.destroy', $book)}}" method="POST">
       @csrf
       @method('DELETE')
       <input type="submit" value="Elimina">
     </form>
+
 
   </body>
 </html>
